5923f4141027c825d1c68f744ab9a9a9a7c0bf6f
Hidai Satoshi <hiddy19970329@gmail.com>
2022-06-21 21:35:21 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.lime.shade900,
+          appBar: AppBar(
+            backgroundColor: Colors.teal[900],
+            title: Text('I Am Poor'),
+          ),
+          body: Center(
+            child: Image(image: AssetImage('images/poorcow.png')),
+          )),
+    ),
+  );
+}
